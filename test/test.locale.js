0087bac2eb37bbff99bee56bec059508baccbef8
ok <liuke0821@163.com>
2022-08-03 01:37:04 +0800

    feat: 1

--- test/test.locale.js
+++ test/test.locale.js
@@ -1,6 +1,7 @@
 const openAPI = require('../dist/index');
 
 openAPI.generateService({
-  schemaPath: './openapi.json',
+  schemaPath: 'https://localhost:7184/swagger/v1/swagger.json',
   serversPath: './servers',
+  enumStyle: 'enum',
 });
